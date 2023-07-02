3f155429a5fd3d25fc334cb4ee0a164ae8d8f958
Prequelcoding <70440857+Codefreak31@users.noreply.github.com>
2023-07-02 20:31:43 +0530

    Create Automation of a message to WhatsApp via Python.py

---
+++ Automation of a message to WhatsApp via Python.py
@@ -0,0 +1,18 @@
+#Now we need to open the pycharm terminal and install pywhatkit in it.
+install:
+  pip install pywhatkit
+
+import pywhatkit
+   #Now we need to make the use of pywhatkit and send the message:
+   pywhatkit.sendwhatmsg(“phone-number”, “The message”, hours, minutes)
+
+#We need to enter the phone number with the country code or else the compiler will pop error.
+
+#Now the output and full code is:
+
+
+#Full Code
+
+import pywhatkit
+
+pywhatkit.sendwhatmsg(“+xyz”, “This is prequelcoding.in”, 13, 49)
